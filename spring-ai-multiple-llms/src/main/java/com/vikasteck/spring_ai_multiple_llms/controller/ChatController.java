5492e8c421d5e5dcd7b5ed96c3d506cb2279c5cb
vikash <vikashvs36@gmail.com>
2024-10-22 00:29:29 +0530

    Chat-client response returning an Entity and list of entity.

--- spring-ai-multiple-llms/src/main/java/com/vikasteck/spring_ai_multiple_llms/controller/ChatController.java
+++ spring-ai-multiple-llms/src/main/java/com/vikasteck/spring_ai_multiple_llms/controller/ChatController.java
@@ -1,11 +1,15 @@
 package com.vikasteck.spring_ai_multiple_llms.controller;
 
+import com.vikasteck.spring_ai_multiple_llms.controller.response.ActorsFilms;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @Slf4j
 @RestController
@@ -23,6 +27,24 @@ public class ChatController {
                 .content();
         log.info(content);
         return content;
+    }
+
+    @GetMapping("/ai/actor")
+    public ActorsFilms actorsFilm() {
+        System.out.println("Fetching Actors movies...");
+        return chatClient.prompt()
+                .user("Generate the filmography for a random actor.")
+                .call()
+                .entity(ActorsFilms.class);
+    }
+
+    @GetMapping("/ai/actors")
+    public List<ActorsFilms> actorsFilmsList() {
+        System.out.println("Fetching Random Actors upcoming 5 movies...");
+        return chatClient.prompt()
+                .user("Generate the upcoming filmographies for a random 5 actor.")
+                .call()
+                .entity(new ParameterizedTypeReference<List<ActorsFilms>>() {});
     }
 
 }
