c51b786e434277b411b8ef2f9ff3b709d203098c
vikash <vikashvs36@gmail.com>
2024-10-21 22:42:28 +0530

    first commit

---
+++ spring-ai-multiple-llms/src/main/java/com/vikasteck/spring_ai_multiple_llms/SpringAiMultipleLlmsApplication.java
@@ -0,0 +1,13 @@
+package com.vikasteck.spring_ai_multiple_llms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAiMultipleLlmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAiMultipleLlmsApplication.class, args);
+	}
+
+}
---
+++ spring-ai-multiple-llms/src/test/java/com/vikasteck/spring_ai_multiple_llms/SpringAiMultipleLlmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vikasteck.spring_ai_multiple_llms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAiMultipleLlmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
